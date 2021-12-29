ee730c0f6250269d8925118bb0bf0c53f0b109d3
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2021-12-29 17:15:07 +0300

    first commit

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".header__burger")
-const menu = document.querySelector(".header__menu")
-
-burger.addEventListener("click", () =>{
-    menu.classList.toggle("active")
-})
